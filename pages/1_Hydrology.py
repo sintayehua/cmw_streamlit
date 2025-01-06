51e311dcdab9d88d4a87f05076dede783cff9468
Sintayehu Adefires Abebe <sentaddis@gmail.com>
2025-01-06 09:37:08 +0300

    Update 1_Hydrology.py

--- pages/1_Hydrology.py
+++ pages/1_Hydrology.py
@@ -47,7 +47,8 @@ with st.expander("See source code"):
             "8": "#f0f0f0",
             "10": "#0064c8"}
         
-        m.add_cog_layer(cog_landcover, colormap=colormap, name="Landcover")
+        #m.add_cog_layer(cog_landcover, colormap=colormap, name="Landcover")
+        m.add_raster(landcover, colormap=colormap, layer_name="Landcover")
         m.add_legend(colors=colors, labels=labels)
         #m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
         #m.add_raster(landcover, colormap="terrain", layer_name="Landcover")
