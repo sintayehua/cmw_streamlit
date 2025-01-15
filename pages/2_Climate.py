ed2318a1f07d1769aaa1f8a56fa7ded0b493cf05
Sintayehu Adefires Abebe <sentaddis@gmail.com>
2025-01-15 10:36:44 +0300

    Update 2_Climate.py

--- pages/2_Climate.py
+++ pages/2_Climate.py
@@ -4,7 +4,7 @@ folium_map = f.Map([33.8994,35.51775], zoom_start=14)
 
 landcover = "https://raw.githubusercontent.com/sintayehua/cmw_streamlit/main/data/landcover.tif"
 
-raster_layer = f.raster_layers.TileLayer(tiles = landcover)
+raster_layer = f.raster_layers.(tiles = landcover)
 raster_layer.add_to(folium_map)
 
 html_map = folium_map._repr_html_()
