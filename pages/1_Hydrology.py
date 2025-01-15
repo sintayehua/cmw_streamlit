7d7edcc3eafce540a7c354b5b8c093796305f0e6
Sintayehu Adefires Abebe <sentaddis@gmail.com>
2025-01-15 10:47:29 +0300

    Update 1_Hydrology.py

--- pages/1_Hydrology.py
+++ pages/1_Hydrology.py
@@ -17,8 +17,8 @@ with st.expander("See source code"):
         soil = "https://raw.githubusercontent.com/sintayehua/cmw_streamlit/main/data/soil.geojson"
         watersheds = "https://raw.githubusercontent.com/sintayehua/cmw_streamlit/main/data/cmw_watersheds.geojson"
         
-        m.add_geojson(boundary, layer_name="CMW boundary")
-        m.add_geojson(watersheds, layer_name="Watersheds")
+        m.add_geojson(boundary, layer_name="CMW boundary", hover_style=None)
+        m.add_geojson(watersheds, layer_name="Watersheds", hover_style=None)
         #m.add_vector(soil, layer_name="Soil", zoom_to_layer=True)
         m.add_points_from_xy(
             climate,
