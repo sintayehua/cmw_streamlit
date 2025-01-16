56ef0d94585b399cc5f14bc6a918612eedaf3355
Sintayehu Adefires Abebe <sentaddis@gmail.com>
2025-01-16 15:09:17 +0300

    Update 2_Climate.py

--- pages/2_Climate.py
+++ pages/2_Climate.py
@@ -1,11 +1,18 @@
 import folium as f
 import streamlit as st
-folium_map = f.Map([33.8994,35.51775], zoom_start=14)
 
+# Create a Folium map centered at a specific location
+folium_map = f.Map([33.8994, 35.51775], zoom_start=14)
+
+# URL to the raster file
 landcover = "https://raw.githubusercontent.com/sintayehua/cmw_streamlit/main/data/landcover.tif"
 
+# Add the raster layer to the Folium map
 raster_layer = f.raster_layers.ImageOverlay(landcover)
 raster_layer.add_to(folium_map)
 
+# Convert the Folium map to HTML
 html_map = folium_map._repr_html_()
-st.markdown(html_map, unsafe_allow_html =True)
+
+# Display the map in Streamlit
+st.markdown(html_map, unsafe_allow_html=True)
