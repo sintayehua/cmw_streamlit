3dfea043691ca251200453f8491188433b9fe7a6
Sintayehu Adefires Abebe <sentaddis@gmail.com>
2025-01-16 15:32:03 +0300

    Update 2_Climate.py

--- pages/2_Climate.py
+++ pages/2_Climate.py
@@ -1,18 +1,23 @@
 import folium as f
 import streamlit as st
+import rasterio
 from streamlit_folium import folium_static
-
-# Create a Folium map centered at a specific location
-folium_map = f.Map([10.551, 37.762], zoom_start=14)
 
 # URL to the raster file
 landcover = "https://raw.githubusercontent.com/sintayehua/cmw_streamlit/main/data/landcover.tif"
+raster = rasterio.open(landcover)
+bounds = raster.bounds
+center = [(bounds.top + bounds.bottom) / 2, (bounds.left + bounds.right) / 2]
+
+# Create a Folium map centered at a specific location
+folium_map = f.Map(location=center, zoom_start=10)
+
 
 # Define the bounds of the raster image
-bounds = [[9.748, 11.354], [36.868, 38.656]]
+#bounds = [[9.748, 11.354], [36.868, 38.656]]
 
 # Add the raster layer to the Folium map
-raster_layer = f.raster_layers.ImageOverlay(image=landcover, bounds=bounds)
+raster_layer = f.raster_layers.ImageOverlay(image=raster, bounds=bounds)
 raster_layer.add_to(folium_map)
 
 # Display the map in Streamlit
