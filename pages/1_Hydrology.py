8d14ecfbfd22e8169efdb624430796b5f85666cf
Sintayehu Adefires Abebe <sentaddis@gmail.com>
2025-01-16 15:47:51 +0300

    Update 1_Hydrology.py

--- pages/1_Hydrology.py
+++ pages/1_Hydrology.py
@@ -14,62 +14,30 @@ with st.expander("See source code"):
         boundary = "https://raw.githubusercontent.com/sintayehua/cmw_streamlit/main/data/cmw_max_boundary.geojson"
         climate = "https://raw.githubusercontent.com/sintayehua/cmw_streamlit/main/data/nma_stations.csv"
         flow = "https://raw.githubusercontent.com/sintayehua/cmw_streamlit/main/data/flow_stations.csv"
-        landcover = "https://raw.githubusercontent.com/sintayehua/cmw_streamlit/main/data/landcover.tif"
-        landcover_full = "https://raw.githubusercontent.com/sintayehua/cmw_streamlit/main/data/LandCover20m2016ESAC.tif"
-        cog_landcover = "https://raw.githubusercontent.com/sintayehua/cmw_streamlit/main/data/landcover_cog.tif"
-        soil = "https://raw.githubusercontent.com/sintayehua/cmw_streamlit/main/data/soil.geojson"
         watersheds = "https://raw.githubusercontent.com/sintayehua/cmw_streamlit/main/data/cmw_watersheds.geojson"
         
         m.add_geojson(boundary, layer_name="CMW boundary", info_mode='on_click')
         m.add_geojson(watersheds, layer_name="Watersheds", info_mode='on_click')
-        #m.add_vector(soil, layer_name="Soil", zoom_to_layer=True)
         m.add_points_from_xy(
             climate,
             x="long",
             y="lat",
-            #color_column="station_name",
-            #icon_names=["gear", "map", "leaf", "globe"],
-            spin=False,
+            layer_name="Weather Stations",
+            icon_names="cloud",
             add_legend=True,
         )
-        # m.add_points_from_xy(
-        #     flow,
-        #     x="lon",
-        #     y="lat",
-        #     #color_column="station_name",
-        #     #icon_names=["gear", "map", "leaf", "globe"],
-        #     spin=False,
-        #     add_legend=True,
-        # )
-        # Add a Legend
-        colors = ['#006400', '#ffbb22','#ffff4c','#f096ff',
-                  '#fa0000', '#b4b4b4','#f0f0f0','#0064c8']
-        labels = ["Trees","Shrubland","Grassland","Cropland",
-                  "Herbaceous wetland", "Bareland", "Built-up", "Open water"]
-        colormap = {
-            "1": "#006400",
-            "2": "#ffbb22",
-            "3": "#ffff4c",
-            "4": "f096ff",
-            "5": "#fa0000",
-            "7": "#b4b4b4",
-            "8": "#f0f0f0",
-            "10": "#0064c8"}
+        m.add_points_from_xy(
+            flow,
+            x="long",
+            y="lat",
+            layer_name="Flow gauges",
+            icon_names="water",
+            add_legend=True,
+        )
         
-        #m.add_cog_layer(cog_landcover, colormap= 'viridis', name="Landcover")
-        #m.add_colormap(cmap=colormap)
-        #m.add_raster(landcover, bands=[1], layer_name="Landcover", vmin=1, vmax=10)
         m.split_map(
             left_layer="ESA WorldCover 2020 S2 FCC", right_layer="ESA WorldCover 2020"
         )
         m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
-        #m.add_legend(colors=colors, labels=labels)
-        #m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
-        #m.add_raster(landcover, colormap="terrain", layer_name="Landcover")
-        #m.add_legend()
-        #m.split_map(
-        #    left_layer=landcover_full, right_layer=landcover
-        #)
-        #m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
-    
+        
 m.to_streamlit(height=700)
