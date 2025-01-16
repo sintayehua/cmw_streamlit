936fd6d7c7f7328bb10aaa977af79006d378a6f3
Sintayehu Adefires Abebe <sentaddis@gmail.com>
2025-01-16 15:56:26 +0300

    Update 1_Hydrology.py

--- pages/1_Hydrology.py
+++ pages/1_Hydrology.py
@@ -23,7 +23,7 @@ with st.expander("See source code"):
             x="long",
             y="lat",
             layer_name="Weather Stations",
-            icon_names="fa-certificate",
+            icon_names="<i class="fa-solid fa-water"></i>",
             add_legend=True,
         )
         m.add_points_from_xy(
