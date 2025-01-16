d9f531d2dda6ce8cc58b507d3100ea570c0cbbae
Sintayehu Adefires Abebe <sentaddis@gmail.com>
2025-01-16 16:39:18 +0300

    Update 1_Hydrology.py

--- pages/1_Hydrology.py
+++ pages/1_Hydrology.py
@@ -12,10 +12,12 @@ with st.expander("See source code"):
         m.add_basemap("HYBRID")
         boundary = "https://raw.githubusercontent.com/sintayehua/cmw_streamlit/main/data/cmw_max_boundary.geojson"
         climate = "https://raw.githubusercontent.com/sintayehua/cmw_streamlit/main/data/nma_stations.csv"
-        watersheds = "https://raw.githubusercontent.com/sintayehua/cmw_streamlit/main/data/cmw_watersheds.geojson"
+        watersheds = "https://raw.githubusercontent.com/sintayehua/cmw_streamlit/main/data/rivers.geojson"
+        rivers = "https://raw.githubusercontent.com/sintayehua/cmw_streamlit/main/data/cmw_watersheds.geojson"
         
         m.add_geojson(boundary, layer_name="CMW boundary", info_mode='on_click')
         m.add_geojson(watersheds, layer_name="Watersheds", info_mode='on_click')
+        m.add_geojson(rivers, layer_name="Watersheds", info_mode='on_click')
         m.add_points_from_xy(
             climate,
             x="long",
