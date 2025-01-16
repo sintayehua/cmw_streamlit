2c410ac8167750944b4f06c0cb949292abe19293
Sintayehu Adefires Abebe <sentaddis@gmail.com>
2025-01-16 16:03:14 +0300

    Update 1_Hydrology.py

--- pages/1_Hydrology.py
+++ pages/1_Hydrology.py
@@ -23,7 +23,7 @@ with st.expander("See source code"):
             x="long",
             y="lat",
             layer_name="Weather Stations",
-            icon_names=asterisk,
+            icon_names="*",
             add_legend=True,
         )
         m.add_points_from_xy(
